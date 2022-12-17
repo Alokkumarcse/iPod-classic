33e4df1e3fd9710763ef117307937cc8d4ee683d
Alok Kumar <alokk13799@gmail.com>
2022-12-17 10:46:33 +0530

    Wheel and screen component added

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+
+import '../styles/App.css';
+import WheelButton from './WheelButton';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> IPOD PLAYER </h1>
+      <WheelButton />
+    </div>
+  );
+}
+
+export default App;
