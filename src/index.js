3661960120f42940ffdb34cde780a9501026de76
Alok Kumar <alokk13799@gmail.com>
2023-01-17 16:00:21 +0530

    overflow bug fixed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client"; 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import App from './components/App';
 
-import App from './components/App/App';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-   <React.StrictMode>
-      <App />
-   </React.StrictMode>
-)
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
